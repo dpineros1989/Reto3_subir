951084de52dfbcea992843f903e454a97df4b48b
dpineros1989 <42304660+dpineros1989@users.noreply.github.com>
2022-10-07 17:17:29 -0500

    Add files via upload
    Pruebas mastertech

---
+++ Reto3/src/main/java/com/usa/g14/reto3/Controlador/ClientControlador.java
@@ -0,0 +1,28 @@
+package com.usa.g14.reto3.Controlador;
+
+import com.usa.g14.reto3.Modelo.Cabin;
+import com.usa.g14.reto3.Modelo.Client;
+import com.usa.g14.reto3.Servicio.CabinService;
+import com.usa.g14.reto3.Servicio.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Client")
+
+public class ClientControlador {
+    @Autowired
+    private ClientService clientService;
+    @GetMapping("/all")
+    public List<Client> getAll(){
+        return clientService.getAll();
+    }
+
+    @PostMapping("/save")
+    public Client save(@RequestBody Client p){
+        return clientService.save(p);
+
+    }
+}
